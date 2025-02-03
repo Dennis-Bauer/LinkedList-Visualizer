17bf5b0d6b2daf0e0a16255497103be119ba0fe7
DennisBauer-Mac <sandwich898.gaming@gmail.com>
2025-02-03 19:20:48 +0100

    This class function like a topper class from the ElementObjekt class. Its stores the object from the Node that gets also represented from this class. Its primary usage is to store the next element in the list and to store the content separate from the ElementObjekt.

--- src/main/java/de/dennis/llvisualizer/ListElement.java
+++ src/main/java/de/dennis/llvisualizer/ListElement.java
@@ -1,4 +1,35 @@
 package de.dennis.llvisualizer;
 
+import de.dennis.llvisualizer.Panes.ElementObjekt;
+
 public class ListElement {
+
+    private int content;
+    private ListElement next = null;
+
+    private final ElementObjekt objekt;
+
+    public ListElement(int content) {
+        this.content = content;
+
+        objekt = new ElementObjekt(content);
+    }
+
+    public ElementObjekt getGraphicObject() {
+        return objekt;
+    }
+
+    public ListElement getNext() {
+        return next;
+    }
+
+    public void setContent(int content) {
+        this.content = content;
+        getGraphicObject().setContent(content);
+    }
+
+    public void setNext(ListElement next) {
+        this.next = next;
+    }
 }
+
