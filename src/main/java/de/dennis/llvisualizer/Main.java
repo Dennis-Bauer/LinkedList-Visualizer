ce1565da73c01a80f227df3c0287a6de95af2de5
Sandwich898Mac <sandwich898.gaming@gmail.com>
2025-02-03 19:03:06 +0100

    Setup default Values. With these values the user can change graphical and functinal things in the Programm.

--- src/main/java/de/dennis/llvisualizer/Main.java
+++ src/main/java/de/dennis/llvisualizer/Main.java
@@ -3,9 +3,23 @@ package de.dennis.llvisualizer;
 import java.io.IOException;
 
 import javafx.application.Application;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
+    public static final double WINDOW_WIDTH = 800;
+    public static final double WINDOW_HEIGHT = 600;
+    public static final String TEXT_FONT = "Arial";
+    public static final double FONT_SIZE = (WINDOW_HEIGHT + WINDOW_WIDTH) * 0.015;
+
+    // Debug/Variables
+    public static final int MAX_INPUT_LENGTH = 3;
+    public static final Color CURRENT_ARROW_COLOR = Color.INDIANRED; // Default: INDIANRED
+    public static final Color FIRST_LAST_ARROW_COLOR = Color.BLUE; // Default: BLUE
+    public static final Color CONTENT_COLOR_CURRENT = Color.GRAY; // Default: GRAY
+    public static final Color CONTENT_COLOR_GOT = Color.WHITE; // Default: WHITE
+    public static final Color CONTENT_COLOR_NOTHING = Color.BLACK; // Default: BLACK
 
     @Override
     public void start(Stage stage) throws IOException {
