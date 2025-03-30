cd23536d894d8f3cde0b2e08c799ace6257ab9f5
DennisBauer-Mac <sandwich898.gaming@gmail.com>
2025-03-30 17:59:53 +0200

    Set up the linked list and added the ‘append’ method.

--- JavaScript-Source/Script/LinkedList.js
+++ JavaScript-Source/Script/LinkedList.js
@@ -1,1 +1,50 @@
-export class LinkedList {}
+import { NextElementTypError } from "./Errors/NextElementTypError.js";
+import { ListElement } from "./ListElement.js";
+
+export class LinkedList {
+  #elments = [];
+  #currentPositon;
+
+  constructor(listContainer) {
+    this.listContainer = listContainer;
+  }
+
+  next() {}
+
+  append(value) {
+    const newElement = new ListElement(value, this.#elments.at(-1));
+
+    this.#addElement(newElement);
+
+    this.#elments.push(newElement);
+  }
+
+  setContent(content) {}
+
+  toFirst() {}
+
+  insert(content) {}
+
+  getContent() {}
+
+  toLast() {}
+
+  remove() {}
+
+  isEmpty() {}
+
+  hasCurrentAccess() {}
+
+  #addElement(listElement) {
+    if (listElement instanceof ListElement) {
+      if (this.#elments.length !== 0) {
+        this.listContainer.insertAdjacentHTML(
+          "beforeend",
+          `<div class="arrow">→</div>`
+        );
+      }
+
+      listElement.addToDOM(this.listContainer);
+    } else throw new NextElementTypError();
+  }
+}
