f35553f3399ad84cdacde9ede5a3fafae23237a5
DennisBauer-Mac <sandwich898.gaming@gmail.com>
2025-03-30 17:41:21 +0200

    Finished the ListElement class; it now stores the reference to the next element.

--- JavaScript-Source/Script/ListElement.js
+++ JavaScript-Source/Script/ListElement.js
@@ -2,16 +2,18 @@ import { ContentTypError } from "./Errors/ContentTypError.js";
 
 export class ListElement {
   divElement;
+  #nextElement;
 
-  constructor(value) {
+  constructor(value, nextElement = undefined) {
     if (typeof value === "number") this.value = value;
     else throw new ContentTypError();
-  }
 
-  addPointer(pointerName) {
-    if (this.divElement !== undefined) {
-      this.divElement.querySelector(".pointer").textContent = pointerName;
-    }
+    if (nextElement instanceof ListElement || nextElement === undefined) {
+      this.#nextElement = nextElement;
+    } else
+      throw new Error(
+        "The next elment from one elemnt has to be an instance from the class ListElement!"
+      );
   }
 
   setContent(content) {
@@ -19,6 +21,12 @@ export class ListElement {
       this.value = content;
       this.divElement.querySelector(".node").textContent = content;
     } else throw new ContentTypError();
+  }
+
+  addPointer(pointerName) {
+    if (this.divElement !== undefined) {
+      this.divElement.querySelector(".pointer").textContent = pointerName;
+    }
   }
 
   addToDOM(parentElement) {
@@ -30,5 +38,18 @@ export class ListElement {
         </div>`
     );
     this.divElement = parentElement.lastElementChild;
+  }
+
+  getNextElement() {
+    return this.#nextElement;
+  }
+
+  setNextElement(newNextElement) {
+    if (newNextElement instanceof ListElement || newNextElement === undefined) {
+      this.#nextElement = newNextElement;
+    } else
+      throw new Error(
+        "The next elment from one elemnt has to be an instance from the class ListElement!"
+      );
   }
 }
