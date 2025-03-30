0f885c76741aa07a1d40c0c9e7807abb50b91b64
DennisBauer-Mac <sandwich898.gaming@gmail.com>
2025-03-30 17:19:34 +0200

    Added a method to the ListElement class to insert elements into the DOM list.

--- JavaScript-Source/Script/ListElement.js
+++ JavaScript-Source/Script/ListElement.js
@@ -1,12 +1,18 @@
 export class ListElement {
+  divElement;
+
   constructor(value) {
     this.value = value;
-    console.log("HAHAHHAHA");
   }
 
-  get htmlString() {
-    return `<div class="node-container">
-                  <div class="node">${this.value}</div>
-              </div>`;
+  addToDOM(parentElement) {
+    parentElement.insertAdjacentHTML(
+      "beforeend",
+      `<div class="node-container">
+            <div class="pointer"></div>
+            <div class="node">${this.value}</div>
+        </div>`
+    );
+    this.divElement = parentElement.lastElementChild; // Speichert das hinzugefügte Element
   }
 }
